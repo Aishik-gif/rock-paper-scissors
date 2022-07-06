829976c3e8e70795481c1fb53ba79a76d7cc80a3
Aishik <aishikd2@gmail.com>
2022-07-06 20:30:08 +0530

    ui added

--- javascript.js
+++ javascript.js
@@ -42,48 +42,63 @@ function playRound(player, computer) {
         return 2;
       }
       break;
-    case undefined:
-    case null:
-      return 3;
-      break;
-    default:
-      return 4;
   }
 }
 
-function game() {
-  let playerScore = 0,
-    computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let player = prompt("Enter your choice");
-    if (player != null) {
-      player = player.toUpperCase();
-      player = player[0] + player.slice(1).toLowerCase();
-    }
-    computer = computerPlay();
-    let decision = playRound(player, computer);
-    if (decision === 0) {
-      alert(`You lose! ${computer} beats ${player}`);
-      computerScore++;
-    } else if (decision === 1) {
-      alert(`You win! ${player} beats ${computer}`);
-      playerScore++;
-    } else if (decision === 2) {
-      alert("Draw!");
-    } else if (decision === 4) {
-      alert("Enter rock, paper, scissors");
-      i--;
-    } else {
-      return;
-    }
-  }
-  if (playerScore > computerScore) {
-    alert(`You Win. \nScore: ${playerScore} : ${computerScore}`);
-  } else if (computerScore > playerScore) {
-    alert(`You Lose. \nScore: ${playerScore} : ${computerScore}`);
-  } else {
-    alert("Draw");
-  }
-}
+const res = document.querySelector(".result");
+const scr = document.querySelector(".score");
+let playerScore = 0,
+  computerScore = 0;
+const btn = document.querySelectorAll("button");
 
-game();
+btn.forEach((key) =>
+  key.addEventListener("click", (e) => {
+    if (playerScore < 5 && computerScore < 5) {
+      let player = e.target.innerText;
+
+      computer = computerPlay();
+      let decision = playRound(player, computer);
+      if (decision === 0) {
+        computerScore++;
+        res.textContent = `You lose! ${computer} beats ${player}`;
+      } else if (decision === 1) {
+        playerScore++;
+        res.textContent = `You win! ${player} beats ${computer}`;
+      } else {
+        res.textContent = "Draw!";
+      }
+
+      scr.textContent = `Player = ${playerScore} || Computer = ${computerScore}`;
+    }
+    if (
+      playerScore + 1 !== computerScore + 1 &&
+      (playerScore === 5 || computerScore === 5)
+    ) {
+      endgame(playerScore, computerScore);
+    }
+  })
+);
+
+function endgame(p, c) {
+  btn.forEach((item) => (item.style.display = "none"));
+  res.style.display = "none";
+  const final = document.createElement("div");
+  if (p > c) final.innerText = "You Won The Game!!";
+  else final.innerText = "You Lost The Game!!";
+  scr.appendChild(final);
+  const re = document.createElement("button");
+  re.innerText = "Replay?";
+  re.style.height = "50px";
+  re.style.borderRadius = '5px';
+  re.style.fontSize = '0.8em';
+  scr.appendChild(re);
+  re.addEventListener("click", () => {
+    (playerScore = 0), (computerScore = 0);
+    btn.forEach((item) => (item.style.display = "inline"));
+    res.style.display = "flex";
+    final.remove();
+    re.remove();
+    res.textContent = '';
+    scr.textContent = '';
+  });
+}
